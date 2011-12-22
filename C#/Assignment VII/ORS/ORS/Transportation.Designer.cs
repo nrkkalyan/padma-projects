5a3997a0558735c026a27a901e93d37899ae23bf
nrkkalyan <nrkkalyan@gmail.com>
2011-12-22 13:46:02 +0100

    part/6

---
+++ C#/Assignment VII/ORS/ORS/Transportation.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ORS
+{
+    partial class Transportation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timeTransportation = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.cmbFrom = new System.Windows.Forms.ComboBox();
+            this.cmbTo = new System.Windows.Forms.ComboBox();
+            this.btnTransportation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "To";
+            // 
+            // timeTransportation
+            // 
+            this.timeTransportation.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timeTransportation.Location = new System.Drawing.Point(158, 155);
+            this.timeTransportation.Name = "timeTransportation";
+            this.timeTransportation.ShowUpDown = true;
+            this.timeTransportation.Size = new System.Drawing.Size(200, 20);
+            this.timeTransportation.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Time";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(158, 35);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(83, 20);
+            this.txtNumber.TabIndex = 5;
+            // 
+            // cmbFrom
+            // 
+            this.cmbFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFrom.FormattingEnabled = true;
+            this.cmbFrom.Location = new System.Drawing.Point(158, 75);
+            this.cmbFrom.Name = "cmbFrom";
+            this.cmbFrom.Size = new System.Drawing.Size(121, 21);
+            this.cmbFrom.TabIndex = 6;
+            // 
+            // cmbTo
+            // 
+            this.cmbTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTo.FormattingEnabled = true;
+            this.cmbTo.Location = new System.Drawing.Point(158, 116);
+            this.cmbTo.Name = "cmbTo";
+            this.cmbTo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTo.TabIndex = 7;
+            // 
+            // btnTransportation
+            // 
+            this.btnTransportation.Location = new System.Drawing.Point(174, 207);
+            this.btnTransportation.Name = "btnTransportation";
+            this.btnTransportation.Size = new System.Drawing.Size(75, 23);
+            this.btnTransportation.TabIndex = 8;
+            this.btnTransportation.Text = "OK";
+            this.btnTransportation.UseVisualStyleBackColor = true;
+            // 
+            // Transportation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 262);
+            this.Controls.Add(this.btnTransportation);
+            this.Controls.Add(this.cmbTo);
+            this.Controls.Add(this.cmbFrom);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timeTransportation);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Transportation";
+            this.Text = "Transportation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker timeTransportation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.ComboBox cmbFrom;
+        private System.Windows.Forms.ComboBox cmbTo;
+        private System.Windows.Forms.Button btnTransportation;
+    }
+}
---
+++ C#/Assignment VII/ORS/ORS/Transportation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ORS
+{
+    public partial class Transportation : Form
+    {
+        
+         //flag to handle the closing of the form
+        private bool closeForm;
+
+        //constructor with one parameter (title of the form)
+        public Transportation(string title)
+        {
+            InitializeComponent();
+            //Other initalizations
+            this.Text = title;
+            closeForm = true;
+             // clear and updated the cmbFrom with Station and select default value Stockholm.
+            cmbFrom.Items.Clear();
+            cmbFrom.Items.AddRange(Enum.GetNames(typeof(Stations)));
+            cmbFrom.SelectedIndex = (int)Stations.Stockholm;
+
+            // clear and updated the cmbTo with Station and select default value Copenhagen.
+            cmbTo.Items.Clear();
+            cmbTo.Items.AddRange(Enum.GetNames(typeof(Stations)));
+            cmbTo.SelectedIndex = (int)Stations.Copenhagen;
+
+        }
+
+         /// <summary>
+        /// Event hadler for formclosing event event of the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Transportation_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (closeForm)
+                e.Cancel = false;
+            else
+                e.Cancel = true;
+        }
+    }
+}
